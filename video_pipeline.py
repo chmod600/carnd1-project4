f3e39f33c6a4a23304f2472064baaf8c16bf4498
Akshay Khole <akshay@reversehack.in>
2017-04-15 20:10:22 -0700

    Binary Warp done

--- video_pipeline.py
+++ video_pipeline.py
@@ -1,100 +1,76 @@
-global current_image, warped_binary, M, Minv, visualization_image, left_fit, right_fit, left_lane_inds, right_lane_inds
+global current_image, binary_color_combined, warped_binary, M, Minv, visualization_image, left_fit, right_fit, left_lane_inds, right_lane_inds
 
-perform_binary_warp()
-fit_polynomial()
-measure_curvature()
-project_lane_on_image()
+def process_image(image):
+    current_image = image
+    extract_combined_binary()
+    perform_binary_warp()
+    fit_polynomial()
+    measure_curvature()
+    project_lane_on_image()
 
-# warped_binary, M, Minv = perform_binary_warp(binary_color_combined)
-# print_gray_image(warped_binary, "Warped Image")
+def extract_combined_binary():
+    ksize = 15
+    gradx = abs_sobel_thresh(current_image, orient='x', sobel_kernel=ksize, thresh=(20, 100))
+    grady = abs_sobel_thresh(current_image, orient='y', sobel_kernel=ksize, thresh=(20, 100))
 
+    mag_binary = mag_thresh(current_image, sobel_kernel = ksize, mag_thresh=(25, 100))
+    dir_binary = dir_threshold(current_image, sobel_kernel = 15, thresh=(0.7, 1.3))
 
-visualization_image, left_fit, right_fix, left_lane_inds, right_lane_inds = fit_polynomial(warped_binary)
+    combined = np.zeros_like(dir_binary)
+    combined[((gradx == 1) & (grady == 1)) | ((mag_binary == 1) & (dir_binary == 1))] = 1
 
-def measure_curvature(binary_warped):
-    # Generate x and y values for plotting
-    # The linespace function generates uniformly spaced vlues from 0 -> binary_warped.shape[0]
-    # ploty containts points along the lane lines. So points along the Y axis
-    ploty = np.linspace(0, binary_warped.shape[0] - 1, binary_warped.shape[0])
-    left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
-    right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
+    binary_color_combined = np.zeros_like(combined)
+    s_binary = hls_select(current_image, thresh=(90, 200))
+    binary_color_combined[((combined == 1) | (s_binary == 1))] = 1
 
-    # This function is going to draw the points
-    # out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
-    # out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
-    # plt.imshow(out_img)
-    # plt.plot(left_fitx, ploty, color='yellow')
-    # plt.plot(right_fitx, ploty, color='yellow')
-    # plt.xlim(0, 1280)
-    # plt.ylim(720, 0)
+def perform_binary_warp(binary_image):
+    # Use input_image for perspective transform
+    binary_image_orig = np.copy(binary_image)
+    src_points_array = np.array([[550, 480], [750, 480], [1100, 700], [320, 700]])
+    dst_points_array = np.array([[200, 100], [1100, 100], [1100, 650], [200, 650]])
 
-    # # Create an image to draw on and an image to show the selection window
-    # out_img = np.dstack((binary_warped, binary_warped, binary_warped))*255
-    window_img = np.zeros_like(out_img)
-    # # Color in left and right line pixels
-    # out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
-    # out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
+    src_points = np.int32(src_points_array)
+    dst_points = np.int32(dst_points_array)
 
-    # Generate a polygon to illustrate the search window area
-    # And recast the x and y points into usable format for cv2.fillPoly()
-    left_line_window1 = np.array([np.transpose(np.vstack([left_fitx-margin, ploty]))])
-    left_line_window2 = np.array([np.flipud(np.transpose(np.vstack([left_fitx+margin, ploty])))])
-    left_line_pts = np.hstack((left_line_window1, left_line_window2))
-    right_line_window1 = np.array([np.transpose(np.vstack([right_fitx-margin, ploty]))])
-    right_line_window2 = np.array([np.flipud(np.transpose(np.vstack([right_fitx+margin, ploty])))])
-    right_line_pts = np.hstack((right_line_window1, right_line_window2))
+    src_points = src_points.reshape((-1,1,2))
+    dst_points = dst_points.reshape((-1,1,2))
 
-    # Draw the lane onto the warped blank image
-    # cv2.fillPoly(window_img, np.int_([left_line_pts]), (0,255, 0))
-    # cv2.fillPoly(window_img, np.int_([right_line_pts]), (0,255, 0))
-    result = cv2.addWeighted(out_img, 1, window_img, 0.3, 0)
-    # plt.imshow(result)
-    # plt.plot(left_fitx, ploty, color='yellow')
-    # plt.plot(right_fitx, ploty, color='yellow')
-    # plt.xlim(0, 1280)
-    # plt.ylim(720, 0)
+    cv2.polylines(binary_image, [src_points], True, (20, 20, 20), 2)
+    cv2.polylines(binary_image, [dst_points], True, (20, 20, 20), 2)
 
-    ########### Start measuring radius in pixel space ###########
-    # Define y-value where we want radius of curvature
-    # I'll choose the maximum y-value, corresponding to the bottom of the image
-    y_eval = np.max(ploty)
-    left_curverad = ((1 + (2*left_fit[0]*y_eval + left_fit[1])**2)**1.5) / np.absolute(2*left_fit[0])
-    right_curverad = ((1 + (2*right_fit[0]*y_eval + right_fit[1])**2)**1.5) / np.absolute(2*right_fit[0])
-    print("left_curverad, right_curverad")
-    print(left_curverad, right_curverad)
-    # Example values: 1926.74 1908.48
+    print_gray_image(binary_image, "Perspective Transform mappings")
 
-    ########### Convert to real world space ###########
-    # Define conversions in x and y from pixels space to meters
-    ym_per_pix = 30/720 # meters per pixel in y dimension
-    xm_per_pix = 3.7/700 # meters per pixel in x dimension
+    # start warping the image
+    src_points = np.float32(src_points_array)
+    dst_points = np.float32(dst_points_array)
 
-    # Fit new polynomials to x, y in world space
-    left_fit_cr = np.polyfit(lefty*ym_per_pix, leftx*xm_per_pix, 2)
-    right_fit_cr = np.polyfit(righty*ym_per_pix, rightx*xm_per_pix, 2)
+    M = cv2.getPerspectiveTransform(src_points, dst_points)
+    Minv = cv2.getPerspectiveTransform(dst_points, src_points)
 
-    # # Calculate the new radii of curvature
-    left_curverad = ((1 + (2*left_fit_cr[0]*y_eval*ym_per_pix + left_fit_cr[1])**2)**1.5) / np.absolute(2*left_fit_cr[0])
-    right_curverad = ((1 + (2*right_fit_cr[0]*y_eval*ym_per_pix + right_fit_cr[1])**2)**1.5) / np.absolute(2*right_fit_cr[0])
-    # Now our radius of curvature is in meters
-    return left_curverad, right_curverad
+    img_size = (binary_image_orig.shape[1], binary_image_orig.shape[0])
+    binary_warped = cv2.warpPerspective(binary_image_orig, M, img_size)
+    return binary_warped, M, Minv
 
 
-def project_lane_on_image(binary_warped, orig_image):
-    warp_zero = np.zeros_like(binary_warped).astype(np.uint8)
-    color_warp = np.dstack((warp_zero, warp_zero, warp_zero))
 
-    # Recast the x and y points into usable format for cv2.fillPoly()
-    pts_left = np.array([np.transpose(np.vstack([left_fitx, ploty]))])
-    pts_right = np.array([np.flipud(np.transpose(np.vstack([right_fitx, ploty])))])
-    pts = np.hstack((pts_left, pts_right))
+binary_color_combined_orig = np.copy(binary_color_combined)
+src_points_array = np.array([[550, 480], [750, 480], [1100, 700], [320, 700]])
+dst_points_array = np.array([[200, 100], [1100, 100], [1100, 650], [200, 650]])
 
-    # Draw the lane onto the binary_warped blank orig_image
-    green_color = (0, 255, 0)
-    cv2.fillPoly(color_warp, np.int_([pts]), green_color)
+src_points = np.int32(src_points_array)
+dst_points = np.int32(dst_points_array)
+src_points = src_points.reshape((-1,1,2))
+dst_points = dst_points.reshape((-1,1,2))
+cv2.polylines(binary_color_combined, [src_points], True, (20, 20, 20), 2)
+cv2.polylines(binary_color_combined, [dst_points], True, (20, 20, 20), 2)
+print_gray_image(binary_color_combined)
 
-    # Warp the blank back to original orig_image space using inverse perspective matrix (Minv)
-    newwarp = cv2.warpPerspective(color_warp, Minv, (orig_image.shape[1], orig_image.shape[0]))
-    # Combine the result with the original orig_image
-    result = cv2.addWeighted(orig_image, 1, newwarp, 0.3, 0)
-    plt.imshow(result)
+# start warping the image
+src_points = np.float32(src_points_array)
+dst_points = np.float32(dst_points_array)
+
+M = cv2.getPerspectiveTransform(src_points, dst_points)
+Minv = cv2.getPerspectiveTransform(dst_points, src_points)
+img_size = (binary_color_combined_orig.shape[1], binary_color_combined_orig.shape[0])
+binary_warped = cv2.warpPerspective(binary_color_combined_orig, M, img_size)
+print_gray_image(binary_warped)
